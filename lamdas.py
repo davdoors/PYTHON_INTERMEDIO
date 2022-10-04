79a6b7a1edd52e49ccc123c05378abfa8d71cd16
davdorado <david.dorado@ngny.tech>
2022-10-04 18:56:58 +0200

    Clase 12

--- lamdas.py
+++ lamdas.py
@@ -1,6 +1,7 @@
 def run():
+    txt = str(input("enter name: "))
     palindrome = lambda name: name == name[::-1]
-    print(palindrome('david'))
+    print(palindrome(txt))
 
 if __name__ == '__main__':
     run()
